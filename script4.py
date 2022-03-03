6bc7317285e40d598a35005c7b4127bb0975ae10
RakhmatulloShamsitdinov <99869797+RakhmatulloShamsitdinov@users.noreply.github.com>
2022-03-03 12:41:07 +0300

    script4

---
+++ script4.py
@@ -0,0 +1,11 @@
+magazin = [sigareti, cofe]
+sigareti = int(input("Enter your age"))
+if sigareti > 18:
+    print ("take your sigaretes")
+else:
+    print(" You cant buy siggateres")
+    continue
+
+
+
+
