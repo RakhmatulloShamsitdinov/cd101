7fadb00f774d00e00a103564415de8142c33ea0c
RakhmatulloShamsitdinov <99869797+RakhmatulloShamsitdinov@users.noreply.github.com>
2022-03-10 13:53:06 +0300

    qwe

--- script8.py
+++ script8.py
@@ -3,15 +3,15 @@
 #     return a + b
 # sum_two(1,2)
 
+# # print(c)
+# n = [1,2,2,1,2]
+# def summa(n):
+#     summ = 0
+#     for q in n:
+#         summ = summ + int(q)
+#     return summ
+# c = summa(n)
 # print(c)
-n = [1,2,2,1,2]
-def summa(n):
-    summ = 0
-    for q in n:
-        summ = summ + int(q)
-    return summ
-c = summa(n)
-print(c)
 
 
     
