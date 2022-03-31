5eed9124d85c0899eb48f075c073ad5c2a34e6a4
RakhmatulloShamsitdinov <99869797+RakhmatulloShamsitdinov@users.noreply.github.com>
2022-03-31 12:29:05 +0300

    Create script9.py
    Массив

---
+++ script9.py
@@ -0,0 +1,31 @@
+# point = []
+# lines = ["Petya",12,43,0]
+
+# print(len(lines))
+# lines.append("Kolya")
+# print(lines[0])
+
+# dict = {"name":"Petya","age": 32,"status":True}
+# print(dict["age"])
+
+# dict = {"name":"Petya","age": 32, "wifes":["Katya","Kate"]}
+# print(dict["wifes"])
+# dict["status"] = "single"
+# print(dict)
+# for k, v in dict.items():
+#     print(k,v)
+# customers =[]
+# for t in range (3):
+#     user_data = input("Enter name,age,product:").split(",")
+#     # customers.append({"name": user[0],"age": user[1], "product":user[2] })
+# for i in customers:
+#     print(customers)
+
+# total_age = 0
+# for i in customers:
+#     total_age = total_age + int(i["age"])
+# avg_age = total_age / len(customers)
+# print(avg_age)
+#     # print("age")
+
+# print(customers)
